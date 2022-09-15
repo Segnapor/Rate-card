6486d3486f166055e9ba00278da7c15dba0344ee
Ademiluyi oluwasegun isaac <104950677+Segnapor@users.noreply.github.com>
2022-09-16 01:36:36 +0530

    Upcard
    Rate card

---
+++ index.js
@@ -0,0 +1,44 @@
+const invoker = document.querySelector('#btn');
+const rates = document.querySelectorAll('.rate');
+const ratings = document.querySelector('#ratings');
+const selected = document.querySelector('#selected');
+const card = document.querySelector('#card');
+const cardTwo = document.querySelector('#card2'); 
+const invokerTwo = document.querySelector('#btn2');
+const clickSubmit = document.querySelector('.press-btn');
+// for all rates buttons use--- foreach.
+let seer;  
+   rates.forEach((rate) => {
+       rate.addEventListener('click', ()=>{
+        seer = rate.innerText;
+         console.log(seer);
+         rates.forEach((rate)=>{
+        rate.classList.remove('active');
+            }); 
+            rate.classList.add('active');
+        });
+    });
+
+    invoker.addEventListener('click', ()=>{
+         if(seer) {          
+        card.style.display = 'none';
+        cardTwo.style.display = 'block';
+        selected.innerText = seer;
+        console.log(selected) ;  
+   }
+    else{
+        card.style.display = 'block';
+        invoker.textContent =  "CHOOSE RATE" ;
+        invoker.style.backgroundColor = 'red';
+        clickSubmit.textContent = "Oops!"
+        clickSubmit.style.color = 'red';
+        clickSubmit.style.backgroundColor = 'white';
+        clickSubmit.style.padding = '5px';
+        clickSubmit.style.borderRadius = '50%';
+        console.log(clickSubmit);
+    }
+});
+invokerTwo.addEventListener('click' , ()=>{
+    cardTwo.style.display = 'none';
+    card.style.display = 'block';
+})
